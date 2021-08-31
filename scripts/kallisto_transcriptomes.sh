0b65aa250f22e6daed00078bf816ae8ceaa030e9
ard329 <44094797+ard329@users.noreply.github.com>
2021-08-31 13:12:08 -0400

    Create kallisto_transcriptomes.sh

---
+++ scripts/kallisto_transcriptomes.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+mkdir -p genomes
+
+# Fetch fasta from Ensembl
+wget -O genomes/Mmul_10.cdna.fa.gz \
+	ftp://ftp.ensembl.org/pub/release-99/fasta/macaca_mulatta/cdna/Macaca_mulatta.Mmul_10.cdna.all.fa.gz
+  
+# Unzip fasta
+gunzip genomes/Mmul_10.cdna.fa.gz 
+
+# Load samtools
+module load contrib/samtools/1.9 
+
+# Index reference sequence 
+samtools faidx genomes/Mmul_10.cdna.fa
+
+# Get all non-Y transcripts (for females)
+grep ENSMMUT genomes/Mmul_10.cdna.fa | grep -v "primary_assembly:Mmul_10:Y" | cut -f1 -d " " | sed -e "s/>//g" > genomes/transcripts_noY
+
+# Generate new fasta file for females
+samtools faidx -r genomes/transcripts_noY genomes/Mmul_10.cdna.fa > genomes/Mmul_10.cdna_noY.fa
+
+# Get all non-PAR transcripts (for males)
+grep ENSMMUT genomes/Mmul_10.cdna.fa | grep -v "CD99 antigen" | cut -f1 -d " " | sed -e "s/>//g" > genomes/transcripts_noPAR
+
+# Generate new fasta file for males
+samtools faidx -r genomes/transcripts_noPAR genomes/Mmul_10.cdna.fa > genomes/Mmul_10.cdna_noPAR.fa
