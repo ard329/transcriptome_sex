74ad7f9ea90d4938743ab6239f37b5780303675e
ard329 <44094797+ard329@users.noreply.github.com>
2021-08-31 13:15:39 -0400

    Update kallisto_transcriptomes.sh

--- scripts/kallisto_transcriptomes.sh
+++ scripts/kallisto_transcriptomes.sh
@@ -9,9 +9,6 @@ wget -O genomes/Mmul_10.cdna.fa.gz \
 # Unzip fasta
 gunzip genomes/Mmul_10.cdna.fa.gz 
 
-# Load samtools
-module load contrib/samtools/1.9 
-
 # Index reference sequence 
 samtools faidx genomes/Mmul_10.cdna.fa
 
@@ -21,8 +18,14 @@ grep ENSMMUT genomes/Mmul_10.cdna.fa | grep -v "primary_assembly:Mmul_10:Y" | cu
 # Generate new fasta file for females
 samtools faidx -r genomes/transcripts_noY genomes/Mmul_10.cdna.fa > genomes/Mmul_10.cdna_noY.fa
 
+# Index female transcriptome for kallisto
+kallisto index -i genomes/Mmul_10.cdna_noY.idx genomes/Mmul_10.cdna_noY.fa
+
 # Get all non-PAR transcripts (for males)
 grep ENSMMUT genomes/Mmul_10.cdna.fa | grep -v "CD99 antigen" | cut -f1 -d " " | sed -e "s/>//g" > genomes/transcripts_noPAR
 
 # Generate new fasta file for males
 samtools faidx -r genomes/transcripts_noPAR genomes/Mmul_10.cdna.fa > genomes/Mmul_10.cdna_noPAR.fa
+
+# Index male transcriptome for kallisto
+kallisto index -i genomes/Mmul_10.cdna_noPAR.idx genomes/Mmul_10.cdna_noPAR.fa
