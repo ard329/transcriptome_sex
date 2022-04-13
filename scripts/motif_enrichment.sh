213a717ea5c47a7cbbf44d483633e3c777ff5ba8
ard329 <44094797+ard329@users.noreply.github.com>
2022-04-13 10:57:07 -0400

    Update motif_enrichment.sh

--- scripts/motif_enrichment.sh
+++ scripts/motif_enrichment.sh
@@ -2,6 +2,6 @@
 
 module load contrib/homer/4.10
 
-findMotifs.pl sb_genes_13regions.txt mmul10 sb_genes_13regions -start -1000 -end 300 -bg all_exp_genes_no_Y.txt -mset vertebrates -nomotif -nogo
+findMotifs.pl list-sex-biased-genes.txt mmul10 output_directory -start -1000 -end 300 -bg list-all-expressed-genes.txt -mset vertebrates -nomotif -nogo
 
-findMotifs.pl sb_genes_13regions_ct.txt mmul10 sb_genes_13regions_ct -start -1000 -end 300 -bg all_exp_genes_no_Y.txt -mset vertebrates -nomotif -nogo
+findMotifs.pl list-sex-biased-genes-cell-type-corrected.txt mmul10 output_directory -start -1000 -end 300 -bg list-all-expressed-genes.txt -mset vertebrates -nomotif -nogo
