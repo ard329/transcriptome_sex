8d3fdbff02b02020e3d6ac51233cac202da6a794
Alex DeCasien <44094797+ardecasien@users.noreply.github.com>
2024-04-22 15:27:45 -0400

    Update motif_enrichment.sh

--- scripts/motif_enrichment.sh
+++ scripts/motif_enrichment.sh
@@ -2,6 +2,8 @@
 
 module load contrib/homer/4.10
 
+loadGenome.pl -name mmul10 -org mmul -fasta mmul10.dna.assem.fa -gtf mmul10.anno.gtf
+
 findMotifs.pl list-sex-biased-genes.txt mmul10 output_directory -start -1000 -end 300 -bg list-all-expressed-genes.txt -mset vertebrates -nomotif -nogo
 
 findMotifs.pl list-sex-biased-genes-cell-type-corrected.txt mmul10 output_directory -start -1000 -end 300 -bg list-all-expressed-genes.txt -mset vertebrates -nomotif -nogo
