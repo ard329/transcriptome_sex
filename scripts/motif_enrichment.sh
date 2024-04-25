008d1532e1cd001a4a4fe347f07bc5e88092a228
Alex DeCasien <44094797+ardecasien@users.noreply.github.com>
2024-04-25 12:03:19 -0400

    Update motif_enrichment.sh

--- scripts/motif_enrichment.sh
+++ scripts/motif_enrichment.sh
@@ -4,6 +4,12 @@ module load contrib/homer/4.10
 
 loadGenome.pl -name mmul10 -org mmul -fasta mmul10.dna.assem.fa -gtf mmul10.anno.gtf
 
+# macaque (primary)
 findMotifs.pl list-sex-biased-genes.txt mmul10 output_directory -start -1000 -end 300 -bg list-all-expressed-genes.txt -mset vertebrates -nomotif -nogo
+findMotifs.pl list-male-biased-genes.txt mmul10 output_directory -start -1000 -end 300 -bg list-all-expressed-genes.txt -mset vertebrates -nomotif -nogo
+findMotifs.pl list-female-biased-genes.txt mmul10 output_directory -start -1000 -end 300 -bg list-all-expressed-genes.txt -mset vertebrates -nomotif -nogo
 
+# macaque (cell-type corrected)
 findMotifs.pl list-sex-biased-genes-cell-type-corrected.txt mmul10 output_directory -start -1000 -end 300 -bg list-all-expressed-genes.txt -mset vertebrates -nomotif -nogo
+findMotifs.pl list-male-biased-genes-cell-type-corrected.txt mmul10 output_directory -start -1000 -end 300 -bg list-all-expressed-genes.txt -mset vertebrates -nomotif -nogo
+findMotifs.pl list-female-biased-genes-cell-type-corrected.txt mmul10 output_directory -start -1000 -end 300 -bg list-all-expressed-genes.txt -mset vertebrates -nomotif -nogo
